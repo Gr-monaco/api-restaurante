91ed681a9c5760b699c034429938f7fc9b4e67ed
Gr-monaco <gr-monaco@hotmail.com>
2022-05-18 20:37:05 -0300

    Cria rota get para login

--- routes/Usuario.js
+++ routes/Usuario.js
@@ -17,14 +17,19 @@ router.post('/', async(req, res)=> {
     }
 })
 
-router.get("/login/:email", getUsuarioByEmail, (req, res)=>{
-    res.json(res.usuario);
+router.get("/login", getUsuarioByEmail, (req, res)=>{
+
+    if(res.usuario[0].password!= req.body.password){
+        res.status(401).json({message:`Wrong password`});
+        return;
+    }
+    res.status(200).json({username: res.usuario[0].username, email: res.usuario[0].email});
 })
 
 async function getUsuarioByEmail(req, res, next){
     let usuario;
     try {
-        usuario = await Usuario.find({email:req.params.email});
+        usuario = await Usuario.find({email:req.body.email});
         if (usuario == null){
             return res.status(404).json({message: `Cannot find entry of ${Usuario.modelName}`})
         }
