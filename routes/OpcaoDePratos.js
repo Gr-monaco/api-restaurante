e3f028ec6b45be50845970b7a22c902a4154d558
Gr-monaco <gr-monaco@hotmail.com>
2022-05-18 00:27:37 -0300

    Adiciona get de um prato por id e função base para outras requisições

--- routes/OpcaoDePratos.js
+++ routes/OpcaoDePratos.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const OpcaoDePrato = require('../models/OpcaoDePrato');
 
+router.get("/:id", getOpcaoDePrato, (req,res)=> {
+    console.log(`GET request from: ${req.baseUrl} UNIQUE`);
+    res.json(res.opcao);
+})
+
 router.get('/', async (req, res) => {
     console.log(`GET request from: ${req.baseUrl}`);
     try {
@@ -12,6 +17,25 @@ router.get('/', async (req, res) => {
     }
 })
 
+
+router.patch('/:id', getOpcaoDePrato, async (req, res)=> {
+    console.log(res.opcao.id);
+    if(req.body.nome != null){
+        console.log(req.body.nome);
+        res.opcao.nome = req.body.nome;
+    }
+    if(req.body.preco != null){
+        res.opcao.preco = req.body.preco;
+    }
+    try{
+        const opAtualizada = await res.opcao.save();
+        res.json(opAtualizada);
+        console.log(`New Entry in Database: ${opAtualizada}`)
+
+    } catch (err){
+        res.status(400).json({message: err.message})
+    }
+})
 
 router.post('/', async (req, res) => {
     console.log(`POST request from: ${req.baseUrl} `);
@@ -27,5 +51,20 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: err.message })
       }
 })
+
+async function getOpcaoDePrato(req, res, next) {
+    let opcao
+    try {
+      opcao = await OpcaoDePrato.findById(req.params.id)
+      if (opcao == null) {
+        return res.status(404).json({ message: `Cannot find entry of ${OpcaoDePrato.modelName}` })
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.opcao = opcao
+    next()
+  }
 
 module.exports = router
