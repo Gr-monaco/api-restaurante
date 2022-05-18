bf2a35a0fbcf3d6c71e43816a073a4dfb60f4381
Gr-monaco <gr-monaco@hotmail.com>
2022-05-18 19:40:36 -0300

    Adiciona rota para usuario

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const db = require('./database')
-const express = require('express')
+const db = require('./database');
+const express = require('express');
 
 
 
-const app = express()
+const app = express();
 
-const cors = require("cors")
-app.use(cors())
+const cors = require("cors");
+app.use(cors());
 
-app.use (express.json())
+app.use (express.json());
 
-const opcaoDePratosRouter = require('./routes/OpcaoDePratos')
-app.use('/opcaoDePrato', opcaoDePratosRouter)
-app.listen(5000)
+const opcaoDePratosRouter = require('./routes/OpcaoDePratos');
+const usuarioRouter = require('./routes/Usuario');
+app.use('/opcaoDePrato', opcaoDePratosRouter);
+app.use('/usuario', usuarioRouter);
+app.listen(5000);
